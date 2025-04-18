bd53cd4b9f43f572e9d093bb1cb96bfd42be8484
hichameait <its.aitbenalla.hichame@gmail.com>
2025-04-18 09:42:50 +0100

    add workscpae of the project

---
+++ inscription.php
@@ -0,0 +1,20 @@
+<?php 
+    
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inscription</title>
+</head>
+<body>
+    <form action="" method="post">
+        <label for="Nom">Le Nom : <input type="text" name="nom"> </label>
+        <label for="Nom">Email : <input type="email" name="email"> </label>
+        <label for="Nom">Mot de passe  : <input type="password" name="pass"></label>
+        <button type="submit" name="singup">Inscrir</button>
+    </form>
+</body>
+</html>
