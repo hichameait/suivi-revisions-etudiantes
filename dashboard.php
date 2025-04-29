d2fe585ef4cd52c1e301278f7c2c3724fecd934b
hichameait <its.aitbenalla.hichame@gmail.com>
2025-04-29 10:04:06 +0100

    ajouter code de statistiques

--- dashboard.php
+++ dashboard.php
@@ -5,6 +5,8 @@
    $pass = "";
    $db_db = "suivi_revisions";
    $host = "localhost";
+   $totalHours = "";
+   $avgNote = "";
    $error = ""; 
 
     if (isset($_SESSION['id'])) {
@@ -17,14 +19,27 @@
         $connect = new PDO("mysql:host=$host;dbname=$db_db", $user, $pass);
         $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
-        $sql = "WHERE date >= NOW() - INTERVAL 7 DAY AND utilisateur_id = :id";
+        $sql = "SELECT SUM(duree) AS total_minutes FROM revisions WHERE utilisateur_id = :id AND date_revision >= NOW() - INTERVAL 7 DAY";;
     
         $stmt = $connect->prepare($sql);
         $stmt->execute([
             ':id'  => $user_id
         ]);
+
+        $result = $stmt->fetch();
+        $totalMinutes = $result["total_minutes"] ?? 0;
+        $totalHours = round($totalMinutes / 60, 2);
+
+        $sql2 = "SELECT AVG(note_comprehension) AS moyenne_notes FROM revisions WHERE utilisateur_id = :id AND date_revision >= NOW() - INTERVAL 7 DAY";
+        $stmt = $connect->prepare($sql2);
+        $stmt->execute([
+            ':id'  => $user_id
+        ]);
+        $result = $stmt->fetch();
+        $avgNote = $result["moyenne_notes"] ? round($result['moyenne_notes'], 2) : 0;
+
     } catch (PDOException $ro) {
-        echo 'Error : {$ro}';
+        echo "Error : {$ro}";
     }
 ?>
 <!DOCTYPE html>
@@ -36,12 +51,12 @@
 </head>
 <body>
     <h1>Statistiques hebdomadaires</h1>
-    <p>Total d’heures de révision : </p>
-    <p>Moyenne des notes de compréhension : </p>
+    <p>Total d’heures de révision : <?= $totalHours ?></p>
+    <p>Moyenne des notes de compréhension : <?= $avgNote ?></p>
 
     <ul>
         <li><a href="./ajouter_revision.php">Ajouter Revision</a></li>
-        <li><a href="./ajouter_revision.php">List Revision</a></li>
+        <li><a href="./revisions.php">List Revision</a></li>
 
 </ul>
 </body>
